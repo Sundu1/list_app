d9b6065e00f9cdb981fcf2a4c2169f3717bbdeef
Admin <sundui.gal63@gmail.com>
2023-04-18 20:36:17 +0800

    table fit

--- frontend/src/pages/List.js
+++ frontend/src/pages/List.js
@@ -1,18 +1,21 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import "../index.css";
+
+//top-[58px]
 
 const List = () => {
   return (
     <div className="">
       <Navbar />
       <Sidebar />
-      <div className="fixed top-[58px] h-full w-full bg-gray-200">
-        <div className="ml-[16em] h-full">
+      <div className="fixed h-full w-full bg-gray-200">
+        <div className="pt-[58px] ml-[16em] h-full">
           <div className="bg-white p-5">New Edit Delete</div>
-          <div className="w-full h-full text-white overflow-y-scroll">
-            <div className="p-5">
-              <div className="p-5 bg-black rounded-lg">
+          <div className=" bg-gray-400 height_full w-full text-white overflow-auto">
+            <div className="p-5 ">
+              <div className="inline-block p-5 mr-5 bg-black rounded-lg">
                 <div className="flex">
                   <div className="px-2">Header</div>
                   <div className="px-2">Header</div>
@@ -20,6 +23,39 @@ const List = () => {
                   <div className="px-2">Header</div>
                   <div className="px-2">Header</div>
                   <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">Header</div>
+                  <div className="px-2">test</div>
+                  <div className="px-2">test</div>
+                  <div className="px-2">test</div>
                 </div>
                 <div className="flex">
                   <div className="px-2">column</div>
