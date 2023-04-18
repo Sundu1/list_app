91f607a26a15ee196a1719709e5be9471d256011
sundui <sundui.gal63@gmail.com>
2023-04-18 13:16:57 +0800

    table

--- frontend/src/pages/List.js
+++ frontend/src/pages/List.js
@@ -4,10 +4,42 @@ import Sidebar from "../components/Sidebar";
 
 const List = () => {
   return (
-    <div>
+    <div className="">
       <Navbar />
       <Sidebar />
-      <div></div>
+      <div className="fixed left-[16em] top-[58px] h-full w-full bg-gray-200">
+        <div className="bg-white p-5 overflow-y-scroll">New Edit Delete</div>
+        <div className="block w-full h-full text-white overflow-x-scroll">
+          <div className="p-5">
+            <div className="p-5 bg-black rounded-lg">
+              <div className="flex">
+                <div className="px-2">Header</div>
+                <div className="px-2">Header</div>
+                <div className="px-2">Header</div>
+                <div className="px-2">Header</div>
+                <div className="px-2">Header</div>
+                <div className="px-2">Header</div>
+              </div>
+              <div className="flex">
+                <div className="px-2">column</div>
+                <div className="px-2">column</div>
+                <div className="px-2">column</div>
+                <div className="px-2">column</div>
+                <div className="px-2">column</div>
+                <div className="px-2">column</div>
+              </div>
+              <div className="flex">
+                <div className="px-2">column</div>
+                <div className="px-2">column</div>
+                <div className="px-2">column</div>
+                <div className="px-2">column</div>
+                <div className="px-2">column</div>
+                <div className="px-2">column</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
